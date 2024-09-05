10e22b5a8e6ac89b445a5b2b70855afb23962d97
yonela-nuba <ynuba@rosebankcollege.co.za>
2024-09-05 20:53:31 +0000

    First First Commit

---
+++ src/main/java/com/mycompany/clasase/Clasase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasase;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Clasase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
